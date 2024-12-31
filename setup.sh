f4e0e0ae8625e66fa880ac9da948af0ed1e21142
Sandro Padin <sandropadin@gmail.com>
2024-12-30 17:31:58 -0800

    add more configs for macos

--- setup.sh
+++ setup.sh
@@ -25,3 +25,22 @@ goku
 
 # Disable accented character suggestions and enables key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Put dock on the right-side of the screen and autohide it
+defaults write com.apple.dock "orientation" -string "left"
+defaults write com.apple.dock "autohide" -bool "true"
+
+# Only show open applications in the dock
+defaults write com.apple.dock "static-only" -bool "true"
+
+# Top right corner: Lock screen
+defaults write com.apple.dock wvous-tr-corner -int 13
+
+# Bottom right corner: Show desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+
+# Bottom left corner: Show desktop
+defaults write com.apple.dock wvous-bl-corner -int 4
+
+# Restart Dock
+killall Dock
